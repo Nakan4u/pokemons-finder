118e5419b8b02d7c755248e6c6c91901f325891f
Grigoriy Nakonechnyi <nakan88@gmail.com>
2017-06-28 23:12:08 +0300

    add web app dist folder, tune webpack config;

--- webpack.config.js
+++ webpack.config.js
@@ -2,48 +2,48 @@ const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const extractSass = new ExtractTextPlugin({
-  filename: "app/web/bundle.css",
-  disable: process.env.NODE_ENV === "development"
+    filename: 'app/web/bundle.css',
+    disable: process.env.NODE_ENV === 'development'
 });
 
 module.exports = {
-  entry: {
-    main: './index.web.js',
+    entry: {
+      main: './index.web.js'
   },
-  devServer: {
-    port: 8082,
-    historyApiFallback: true
+    devServer: {
+      port: 8082,
+      historyApiFallback: true
   },
-  module: {
-    loaders: [
-      {
-        test: /\.css(\.js)?$/,
-        loaders: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.css\.js$/,
-        loader: 'css-js-loader',
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015', 'react'],
+    module: {
+      loaders: [
+        {
+            test: /\.css(\.js)?$/,
+            loaders: ['style-loader', 'css-loader']
         },
-      },
-    ],
+        {
+            test: /\.css\.js$/,
+            loader: 'css-js-loader'
+        },
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+            presets: ['es2015', 'react']
+        }
+        }
+    ]
   },
-  resolve: {
-    alias: {
-      'react-native': 'react-native-web',
-      'react-router-native': 'react-router-dom'
-    },
+    resolve: {
+      alias: {
+        'react-native': 'react-native-web',
+        'react-router-native': 'react-router-dom'
+    }
   },
-  output: {
-    filename: 'app/web/bundle.js'
+    output: {
+      filename: 'dist/bundle.js'
   },
-  plugins: [
-    extractSass
+    plugins: [
+      extractSass
   ]
 };
